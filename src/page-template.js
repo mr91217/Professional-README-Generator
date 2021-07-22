e74961f7d52bcb69af3038b2453831df0be75909
ChunYu Chang <mr91217@gmail.com>
2021-07-22 15:50:17 -0400

    edit RM

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,10 @@
 function generateReadme(answers) {
-    return `
+   return `
 <h1 align="center">${answers.projectTitle} </h1>
-  
+ 
 ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
 ## Description
-   ${answers.description}
+  ${answers.description}
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
@@ -14,25 +14,25 @@ function generateReadme(answers) {
 - [Tests](#tests)
 - [Questions](#questions)
 ## Installation
-   ${answers.installation}
+  ${answers.installation}
 ## Usage
-   ${answers.usage}
+  ${answers.usage}
 ## License
 ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
 <br />
 This application is covered by the ${answers.license} license. 
 ## Contributing
-   ${answers.contributing}
+  ${answers.contributing}
 ## Tests
-✏️ ${answers.tests}
+ ${answers.tests}
 ## Questions
-  ${answers.questions}<br />
+ ${answers.questions}<br />
 <br />
-:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
 <br />
-✉️ Email me with any questions: ${answers.email}<br /><br />
-_This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 
-    `;
-  }
-  
-  module.exports = generateReadme;
+Email me with any questions: ${answers.email}<br /><br />
+This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 
+   `;
+ }
+ 
+ module.exports = generateReadme;
